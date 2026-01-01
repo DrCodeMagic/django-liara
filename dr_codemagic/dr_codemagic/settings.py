abb047f26055fc81053e811992c38585867613ef
sepehr.sr <sepehrsarraf98@gmail.com>
2026-01-01 17:51:26 +0330

    Update settings.py

--- dr_codemagic/dr_codemagic/settings.py
+++ dr_codemagic/dr_codemagic/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
