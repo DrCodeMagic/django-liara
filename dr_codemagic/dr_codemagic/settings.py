840bbeab9e6be7c0a69fa837d73694bb8479e9f3
sepehr.sr <sepehrsarraf98@gmail.com>
2026-01-05 17:58:15 +0330

    Update settings.py

--- dr_codemagic/dr_codemagic/settings.py
+++ dr_codemagic/dr_codemagic/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)%7f)g0ls%cvby_92)*!k+5y0_5tupjzbizu!^lqz#us6r&(7@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['dr-codemagic.liara.run']
 
